9504d175cc761604c7e6402f88bb559f6b470e5d
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-07 09:58:06 -0500

    Lesson03 Complete

--- UnitConverter-lesson01/UnitConverter/ViewController.swift
+++ UnitConverter-lesson01/UnitConverter/ViewController.swift
@@ -7,16 +7,20 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 10
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 10
     }
-    
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "N°C"    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // convert and display temperature
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
