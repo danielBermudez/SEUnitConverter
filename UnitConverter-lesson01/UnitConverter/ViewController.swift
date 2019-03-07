66da58a40510bf563d3c91435e7c8ab5b8b5ebbd
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-07 08:12:19 -0500

    Lesson 01 Assignment

--- UnitConverter-lesson01/UnitConverter/ViewController.swift
+++ UnitConverter-lesson01/UnitConverter/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 211))
+        label.center = CGPoint(x: view.center.x , y: view.center.y+50)
+        label.text = "Programmatical Label"
+        self.view.addSubview(label)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,4 +25,5 @@ class ViewController: UIViewController {
     }
 
 }
+
 
