2fd7113db2353a4f93d3381928b8b22cad5fc774
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-08 15:14:36 -0500

    Lesson09 Assignment  Complete

--- UnitConverter-lesson01/UnitConverter/ViewController.swift
+++ UnitConverter-lesson01/UnitConverter/ViewController.swift
@@ -33,13 +33,42 @@ class ViewController: UIViewController, UIPickerViewDelegate {
   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let defaultPickerRow = celsiusPicker.numberOfRows(inComponent: 0)/2
+        let defaultPickerRow = validateDefaultTemperature()
         celsiusPicker.selectRow(defaultPickerRow, inComponent: 0, animated: false)
         pickerView(celsiusPicker, didSelectRow: defaultPickerRow, inComponent: 0)
     }
+    func validateDefaultTemperature()-> Int{
+        var defaultPickerRow = Int()
+        
+        if UserDefaults.standard.integer(forKey: "defaultDegree") != 0{
+            let rowValue = UserDefaults.standard.integer(forKey: "defaultDegree")
+            for i in 0 ..< celsiusPicker.numberOfRows(inComponent: 0){
+                if temperatureRange!.values[i] == rowValue{
+                    defaultPickerRow = i
+                }
+                
+            }
+        } else {
+            defaultPickerRow = celsiusPicker.numberOfRows(inComponent: 0)/2
+        }
+        return defaultPickerRow
+    }
     
-
+    @IBAction func setDefaultStartingTemperature(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Change default temperature value:", message: "Please enter the new default degree to show:", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            let textField = alert.textFields![0]
+            if let newDegree = textField.text{
+           UserDefaults.standard.set(newDegree, forKey: "defaultDegree")
+            }else{
+                UserDefaults.standard.set(self.celsiusPicker.numberOfRows(inComponent: 0)/2, forKey: "defaultDegree")
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
