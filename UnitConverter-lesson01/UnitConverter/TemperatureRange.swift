51bb7278c77768f4608a2e2e810ac7a33a530df7
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-08 09:01:36 -0500

    Lesson07 Incomplete

---
+++ UnitConverter-lesson01/UnitConverter/TemperatureRange.swift
@@ -0,0 +1,13 @@
+//
+//  TemperaturRange.swift
+//  UnitConverter
+//
+//  Created by Daniel Bermudez on 3/7/19.
+//  Copyright © 2019 Your School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TemperatureRange {
+ 
+}
